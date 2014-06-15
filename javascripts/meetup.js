d4d5e662b71ef9a202b67cdf89afd5a336e03f9b
stanzheng <dev.stanzheng@gmail.com>
2014-06-14 23:55:20 -0400

    stricter linting

--- javascripts/meetup.js
+++ javascripts/meetup.js
@@ -1,4 +1,4 @@
-var foo = $.ajax({
+$.ajax({
     type: 'GET',
     dataType: 'jsonp',
     url: 'http://api.meetup.com/2/groups',
@@ -6,20 +6,14 @@ var foo = $.ajax({
         key: '5f2a61211f8735c1f612b661c263a18',
         group_urlname: 'NorfolkJS',
         fields: 'sponsors',
-        sig_id: true,
+        sig_id: true
     }
 })
-.done(function (json) {
-  $.each( json.results[0].sponsors, function( key, val ) {
-    //items.push( "<li id='" + key + "'>" + val + "</li>" );
-    if(!val.hasOwnProperty("redeem")){
-         var element = $('<a>', {
-                href: val.url,
-              html: $('<img>', {
-                src: val.image_url
-              })
-          });
-         $("#sponsors").append(element);
-      }
-   });
-});
+    .done(function (json) {
+        $.each(json.results[0].sponsors, function (key,val) {
+            if (!val.hasOwnProperty("redeem")) {
+                var element = $('<a>', {href: val.url, html: $('<img>', {src: val.image_url})});
+                $("#sponsors").append(element);
+            }
+        });
+    });
