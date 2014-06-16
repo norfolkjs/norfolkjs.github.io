db42630b9c31e10b7d36bb58d1ad2c11ae51899a
stanzheng <dev.stanzheng@gmail.com>
2014-06-16 01:33:47 -0400

    using temporary CDN link to momemntjs

--- javascripts/events.js
+++ javascripts/events.js
@@ -12,11 +12,15 @@ $.ajax({
       function (json) {
         console.log(json);
         $.each(json.results, function (key,val) {
-            var name = $('<a>', {href: val.event_url, html: $('<h2>', {text: val.name})});
+            var name = $('<a>', {href: val.event_url, html: $('<h3>', {text: val.name})});
             var info = $(val.description).text();
-            info = jQuery.trim(info).substring(0, 200).split(" ").slice(0, -1).join(" ") + "...";
+            info = jQuery.trim(info).substring(0, 250).split(" ").slice(0, -1).join(" ") + "...";
             var description = $('<p>', {text: info});
+            moment.lang("en");
+            var date = moment(val.time).format("dddd, MMMM Do , h:mm a");
+            var rsvp = $('<h2>', {text: "Join " + val.yes_rsvp_count + " ninjas on" });
             var container = $('<div>', {html:name, class: "event"});
+            container.append(rsvp);
             container.append(description);
             $(".events").append(container);
         });
