c5410f069a11d1f0d933e47ad85f23ae59802274
Stanley Zheng <szhen002@odu.edu>
2014-06-16 09:54:49 -0400

    hot patch.. for #40

--- javascripts/meetup.js
+++ javascripts/meetup.js
@@ -3,7 +3,7 @@ $.ajax({
     dataType: 'jsonp',
     url: 'http://api.meetup.com/2/groups',
     data: {
-        key: '5f2a61211f8735c1f612b661c263a18',
+        key: '1f5c35a1603866a50862ee2379',
         group_urlname: 'NorfolkJS',
         fields: 'sponsors',
         sig_id: true
