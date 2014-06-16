d6e135420ece9cc36ab1eb26fa810dd6202371fd
stanzheng <dev.stanzheng@gmail.com>
2014-06-16 01:05:51 -0400

    description truncated

--- javascripts/events.js
+++ javascripts/events.js
@@ -5,7 +5,6 @@ $.ajax({
     data: {
         key: '5f2a61211f8735c1f612b661c263a18',
         group_urlname: 'NorfolkJS',
-        fields: 'sponsors',
         sig_id: true
     }
 })
@@ -13,9 +12,12 @@ $.ajax({
       function (json) {
         console.log(json);
         $.each(json.results, function (key,val) {
-            var element = $('<a>', {href: val.event_url, html: $('<h2>', {text: val.name})});
-            var container = $('<div>',{html:element, class: "event"});
-            console.log(element);
+            var name = $('<a>', {href: val.event_url, html: $('<h2>', {text: val.name})});
+            var info = $(val.description).text();
+            info = jQuery.trim(info).substring(0, 200).split(" ").slice(0, -1).join(" ") + "...";
+            var description = $('<p>', {text: info});
+            var container = $('<div>', {html:name, class: "event"});
+            container.append(description);
             $(".events").append(container);
         });
     });
