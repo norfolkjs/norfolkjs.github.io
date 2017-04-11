3fdbb68fc107c922b79112dabaf4207ee894519f
Stanley Zheng <dev.stanzheng@gmail.com>
2017-04-11 14:51:51 -0400

    prettify and update the signed url

--- javascripts/sponsors.js
+++ javascripts/sponsors.js
@@ -1,17 +1,18 @@
 $.ajax({
-  type: 'GET',
-  dataType: 'jsonp',
-  url: 'http://api.meetup.com/2/groups?radius=25.0&order=id&group_urlname=Norfolkjs&desc=false&offset=0&format=json&page=20&fields=sponsors%2C&signed=true&sig_id=81917392&sig=f4fe3cdddb7d11559f1c0b02c44f8520e9485d84',
+  type: "GET",
+  dataType: "jsonp",
+  url: "https://api.meetup.com/2/groups?offset=0&format=json&group_urlname=norfolkjs&photo-host=public&page=20&radius=25.0&fields=sponsors&order=id&desc=false&sig_id=215788584&sig=be25f93d13098f6075992b9f461ae6db7c7fe9a1",
   data: {}
-})
-
-.done(function (json) {
+}).done(function(json) {
   //add class, set height, width is auto
-  $.each(json.results[0].sponsors, function (key,val) {
+  $.each(json.results[0].sponsors, function(key, val) {
     var image;
     if (!val.hasOwnProperty("redeem")) {
-        image = $('<a>', {href: val.url, html: $('<img>', {src: val.image_url, alt: val.name})});
-        $(".sponsors").append(image);
+      image = $("<a>", {
+        href: val.url,
+        html: $("<img>", { src: val.image_url, alt: val.name })
+      });
+      $(".sponsors").append(image);
     }
   });
 });
