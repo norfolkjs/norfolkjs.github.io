d5d53084616a1e2a45fc5b25a09ca35f1b6c4879
Brian Reed <iambriansreed@gmail.com>
2023-12-13 02:54:23 -0500

    build fix, people update

--- src/build.js
+++ src/build.js
@@ -49,34 +49,32 @@ const copyRecursiveSync = function (src, dest) {
     }
 };
 
-export default async () => {
-    // 1. delete existing build folder
+// 1. delete existing build folder
 
-    if (existsSync(buildToPath)) {
-        deleteFolderRecursive(buildToPath);
-    }
+if (existsSync(buildToPath)) {
+    deleteFolderRecursive(buildToPath);
+}
 
-    // 2. create the build folder
+// 2. create the build folder
 
-    mkdirSync(buildToPath);
+mkdirSync(buildToPath);
 
-    // 3. copy static files to copy
+// 3. copy static files to copy
 
-    copyRecursiveSync(join(baseDir, 'static'), buildToPath);
+copyRecursiveSync(join(baseDir, 'static'), buildToPath);
 
-    // 4. make pages and write to build folder
+// 4. make pages and write to build folder
 
-    const routes = getRoutes();
+const routes = getRoutes();
 
-    for (const [route, filePath] of Object.entries(routes)) {
-        const pageContent = await buildPage(filePath, route);
+for (const [route, filePath] of Object.entries(routes)) {
+    const pageContent = await buildPage(filePath, route);
 
-        const pageDirPath = join(buildToPath, route);
+    const pageDirPath = join(buildToPath, route);
 
-        if (!existsSync(pageDirPath)) mkdirSync(pageDirPath, { recursive: true });
+    if (!existsSync(pageDirPath)) mkdirSync(pageDirPath, { recursive: true });
 
-        const pageBuildPath = join(buildToPath, route, 'index.html');
+    const pageBuildPath = join(buildToPath, route, 'index.html');
 
-        writeFileSync(pageBuildPath, pageContent);
-    }
-};
+    writeFileSync(pageBuildPath, pageContent);
+}
